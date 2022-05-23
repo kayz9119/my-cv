c031ea9035c36e19ea05bedbff24a934301cf872
Kerem Arslantürk <solanahtarigibiyim@gmail.com>
2022-05-23 03:04:39 +0300

    profile card design completed

---
+++ src/components/ProfileCard.js
@@ -0,0 +1,21 @@
+import {BsGithub, BsInstagram, BsLinkedin, BsTwitter} from "react-icons/bs";
+
+export default function ProfileCard () {
+    return (
+        <div className="profile-card">
+            <div className="profile-image">
+                <img src="https://pbs.twimg.com/profile_images/1510473497654575107/fkxhILvd_400x400.jpg" alt="Profile Image"/>
+            </div>
+            <div className="profile-info">
+                <h1>Kerem Arslantürk</h1>
+                <p>Software Engineer</p>
+                <div className="social-icons">
+                    <BsGithub />
+                    <BsTwitter />
+                    <BsLinkedin />
+                    <BsInstagram />
+                </div>
+            </div>
+        </div>
+    )
+};
