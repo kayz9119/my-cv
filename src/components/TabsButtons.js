2ba3d9ca3504db37b835d26ef440e9162691efd7
Kerem Arslantürk <solanahtarigibiyim@gmail.com>
2022-05-25 02:01:21 +0300

    Tabs' navigation is completed and made responsive

--- src/components/TabsButtons.js
+++ src/components/TabsButtons.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 export default function TabsButtons () {
+    const [active, setActive] = useState(0);
+    const handleActive = (id) => {
+        setActive(id);
+    }
     return (
-        <div>
-            Tabs Buttons
+        <div className="profile-tabs">
+            <div className={ active === 0 ? 'tab-button active' : 'tab-button' } onClick={() => handleActive(0)} >
+                Portfolio
+            </div>
+            <div className={ active === 1 ? 'tab-button active' : 'tab-button' } onClick={() => handleActive(1)} >
+                Skills
+            </div>
+            <div className="indicator"></div>
         </div>
     )
 }
