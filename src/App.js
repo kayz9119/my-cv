dc4743a28a404bfa155e0813382326f135405ed9
Kerem Arslantürk <solanahtarigibiyim@gmail.com>
2022-05-26 03:51:19 +0300

    Created Context to reach active tab from App.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,28 @@ import ProfileFacts from "./components/ProfileFacts";
 import ProfileButtons from "./components/ProfileButtons";
 import TabsButtons from "./components/TabsButtons";
 import TabsContent from "./components/TabsContent";
+import {MainContext} from "./components/MainContext";
+import { useState } from "react";
+
+
 function App() {
+  const [active, setActive] = useState(0);
+
+  const data = {
+    active,
+    setActive
+  }
   return (
-    <div className="container">
-      <div className="btn-change-theme">Button</div>
+    <MainContext.Provider value={data}>
+      <div className="container">
+        <div className="btn-change-theme">Button</div>
         <ProfileCard />
         <ProfileFacts />
         <ProfileButtons />
         <TabsButtons />
         <TabsContent />
-    </div>
+      </div>
+    </MainContext.Provider>
   );
 }
 
